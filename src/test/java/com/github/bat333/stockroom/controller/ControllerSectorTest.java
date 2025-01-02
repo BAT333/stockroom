7b810ad51a2e1faa31e5f7ac30d620d19bd5ac86
BAT333 <rafaelolivais09@hotmail.com>
2025-01-02 20:11:16 -0300

    Automated testing done in the controller sector

--- src/test/java/com/github/bat333/stockroom/controller/ControllerSectorTest.java
+++ src/test/java/com/github/bat333/stockroom/controller/ControllerSectorTest.java
@@ -1,5 +1,7 @@
 package com.github.bat333.stockroom.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.bat333.stockroom.domain.Sector;
 import com.github.bat333.stockroom.model.DataAllSector;
@@ -37,6 +39,10 @@ class ControllerSectorTest {
     @Autowired
     private SectorRepository repository;
 
+    private static final String BASE_URL = "/api/sector";
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+
 
     @Test
     @DisplayName("Scenario 01: Attempting to register a sector with invalid data (empty JSON) should return 400 Bad Request")
@@ -48,7 +54,7 @@ class ControllerSectorTest {
         //ACT
 
         var response = mvc.perform(
-                post("/api/sector")
+                post(BASE_URL)
                         .content(json)
                         .contentType(MediaType.APPLICATION_JSON)
         ).andReturn().getResponse();
@@ -67,15 +73,22 @@ class ControllerSectorTest {
 
         //ACT
         var response = mvc.perform(
-                post("/api/sector")
+                post(BASE_URL)
                         .content(jsonDto.write(dataSector).getJson())
                         .contentType(MediaType.APPLICATION_JSON)
         ).andReturn().getResponse();
 
         //ASSERT
         ObjectMapper objectMapper = new ObjectMapper();
-        String jsonContent = objectMapper.writeValueAsString(new DataAllSector(1L, dataSector.sector(), dataSector.shelf(), dataSector.column(), dataSector.row(), List.of()));
 
+        String responseBody = response.getContentAsString();
+
+
+        JsonNode jsonNode = objectMapper.readTree(responseBody);
+
+        long id = jsonNode.get("id").asLong();
+
+        var jsonContent = this.ContentEquals(null,new DataAllSector(id, dataSector.sector(), dataSector.shelf(), dataSector.column(), dataSector.row(), List.of()));
         Assertions.assertEquals(201, response.getStatus());
         Assertions.assertEquals(jsonContent, response.getContentAsString());
     }
@@ -90,12 +103,11 @@ class ControllerSectorTest {
         //ACT
 
         var response = mvc.perform(
-                get("/api/sector")
+                get(BASE_URL)
         ).andReturn().getResponse();
 
         //ASSERT
 
-        ObjectMapper objectMapper = new ObjectMapper();
         String expectedJsonContent = objectMapper.writeValueAsString(emptyPage);
         Assertions.assertEquals(200, response.getStatus());
         Assertions.assertEquals(expectedJsonContent, response.getContentAsString());
@@ -105,19 +117,17 @@ class ControllerSectorTest {
     @DisplayName("Scenario 04: Should return sector details when searching by sector ID")
     void getSectorScenario04() throws Exception {
         // ARRANGE
-        DataSector dataSector = new DataSector("sector", "shelf", "column", "rom");
-        repository.save(new Sector(dataSector));
+        var sector = this.register();
 
         // ACT
         var response = mvc.perform(
-                get("/api/sector/1")
+                get(BASE_URL+"/"+sector.getId())
         ).andReturn().getResponse();
 
         // ASSERT
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonContent = objectMapper.writeValueAsString(new DataAllSector(1L, dataSector.sector(), dataSector.shelf(), dataSector.column(), dataSector.row(), List.of()));
 
 
+        var jsonContent = this.ContentEquals(sector,null);
         Assertions.assertEquals(200, response.getStatus());
 
 
@@ -128,38 +138,49 @@ class ControllerSectorTest {
     @DisplayName("Scenario 05: Should update sector details when updating by sector ID")
     void updateSectorScenario05() throws Exception {
         // ARRANGE
-        DataSector dataSector = new DataSector("sector", "shelf", "column", "rom");
-        DataSector dataSectorNew = new DataSector("sector2", "shelf2", "column2", "rom2");
-        repository.save(new Sector(dataSectorNew));
+        var sector = this.register();
+        DataSector dataSector = new DataSector("sector2", "shelf2", "column2", "rom2");
 
         // ACT
         var response = mvc.perform(
-                patch("/api/sector/1")
+                patch(BASE_URL+"/"+sector.getId())
                         .content(jsonDto.write(dataSector).getJson())
                         .contentType(MediaType.APPLICATION_JSON)
         ).andReturn().getResponse();
 
         // ASSERT
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonContent = objectMapper.writeValueAsString(new DataAllSector(1L, dataSector.sector(), dataSector.shelf(), dataSector.column(), dataSector.row(), List.of()));
+        var jsonContent = this.ContentEquals(sector,new DataAllSector(sector.getId(),dataSector.sector(),dataSector.shelf(),dataSector.column(),dataSector.row(),sector.listPart()));
 
         Assertions.assertEquals(200, response.getStatus());
         Assertions.assertEquals(jsonContent, response.getContentAsString());
     }
+
+
     @Test
     @DisplayName("Scenario 06: Should delete a sector when deleting by sector ID")
     void deleteSectorScenario06() throws Exception {
         // ARRANGE
-        DataSector dataSectorNew = new DataSector("sector2", "shelf2", "column2", "rom2");
-        repository.save(new Sector(dataSectorNew));
-
+        var sector = this.register();
         // ACT
         var response = mvc.perform(
-                delete("/api/sector/1")
+                delete(BASE_URL+"/"+sector.getId())
         ).andReturn().getResponse();
 
         // ASSERT
         Assertions.assertEquals(204, response.getStatus());
+    }
+
+    private Sector register() {
+        DataSector dataSectorNew = new DataSector("sector2", "shelf2", "column2", "rom2");
+        return  repository.save(new Sector(dataSectorNew));
+    }
+
+    private String ContentEquals(Sector sector, DataAllSector dataSector) throws JsonProcessingException {
+        if(dataSector == null){
+            return objectMapper.writeValueAsString(new DataAllSector(sector.getId(), sector.getSectors(), sector.getShelf(), sector.getColumn(), sector.getRow(),sector.listPart()));
+        }else{
+            return objectMapper.writeValueAsString(new DataAllSector(dataSector.id(), dataSector.sector(), dataSector.shelf(), dataSector.column(), dataSector.row(), List.of()));
+        }
     }
 
 }
