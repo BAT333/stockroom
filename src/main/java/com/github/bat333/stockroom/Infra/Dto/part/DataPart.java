5360610070febdb96f3625e1470f2e4c3ad049fd
Bat333 <rafaelolivais09@hotmail.com>
2025-02-19 17:44:24 -0300

    Infra: Creating DTO

---
+++ src/main/java/com/github/bat333/stockroom/Infra/Dto/part/DataPart.java
@@ -0,0 +1,21 @@
+package com.github.bat333.stockroom.Infra.Dto.part;
+
+import com.github.bat333.stockroom.Domain.Entities.part.Part;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record DataPart(
+        @NotNull
+        Long cod,
+        @NotNull
+        String name,
+        @NotNull
+        byte[] image,
+        @NotNull
+        @Positive
+        double amount
+) {
+    public DataPart(Part part) {
+        this(part.getCod(),part.getName(), part.getImage(), part.getAmount());
+    }
+}
