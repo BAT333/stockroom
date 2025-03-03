4eb7d1568492b9bedd2bbbc0f27b588fdbb975c5
Bat333 <rafaelolivais09@hotmail.com>
2025-03-03 19:59:19 -0300

    delete

--- src/main/java/com/github/bat333/stockroom/start/Application/UseCases/Part/contract/CreatePart.java
+++
@@ -1,8 +0,0 @@
-package com.github.bat333.stockroom.start.Application.UseCases.Part.contract;
-
-import com.github.bat333.stockroom.start.Domain.Entities.part.Part;
-
-public interface CreatePart {
-    //example
-    Part execute(Part part) throws RuntimeException;
-}
